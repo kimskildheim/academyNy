2ade9283a6e0d230bacc3749187e248a82aaf761
kim skildheim <kimskildheim@gmail.com>
2016-02-11 15:14:17 +0100

    Refactoring RootController

--- src/main/java/no/kds/academy/service/DataForMainPageService.java
+++ src/main/java/no/kds/academy/service/DataForMainPageService.java
@@ -3,25 +3,51 @@ package no.kds.academy.service;
 import no.kds.academy.Util.TrainingSessionListManupulation;
 import no.kds.academy.domain.TrainingSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class DataForMainPageService {
-
-    Map<String, List<TrainingSession>> dataForView = new HashMap<>();
 
     @Autowired
     TrainingService trainingService;
 
-    public  Map<String, List<TrainingSession>> dataForLastThreeTrainingSessions(){
+    Map<String, List<TrainingSession>> dataForView = new HashMap<>();
 
+    public Map<String, List<TrainingSession>> getTrainingSessionData(){
+        dataForLastThreeTrainingSessions();
+        dataFromAllTrainingSessions();
+
+        return dataForView;
+    }
+
+    private  Map<String, List<TrainingSession>> dataForLastThreeTrainingSessions(){
         List<TrainingSession> listOfTrainingSessions = trainingService.findAllTrainingSessions();
-
         List<TrainingSession> lastThreeTrainingSessions = TrainingSessionListManupulation.listOutLastThreeTrainingsSessins(listOfTrainingSessions);
         dataForView.put("lastThreeTrainingSessions", lastThreeTrainingSessions);
 
         return dataForView;
     }
+
+    private Map<String, List<TrainingSession>> dataFromAllTrainingSessions(){
+        List<TrainingSession> listOfTrainingSessions = trainingService.findAllTrainingSessions();
+        dataForView.put("listOfTrainingSessions", listOfTrainingSessions);
+
+        return dataForView;
+    }
+
+    public Map<String, String> getTodayDate(){
+        Map<String, String> dateForView = new HashMap<>();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+        Calendar calendar = Calendar.getInstance();
+        dateForView.put("today", dateFormat.format(calendar.getTime()));
+        return dateForView;
+    }
+
 }
--- src/main/java/no/kds/academy/web/controller/RootController.java
+++ src/main/java/no/kds/academy/web/controller/RootController.java
@@ -1,35 +1,22 @@
 package no.kds.academy.web.controller;
 
-import no.kds.academy.Util.TrainingSessionListManupulation;
-import no.kds.academy.domain.TrainingSession;
-import no.kds.academy.service.TrainingService;
+import no.kds.academy.service.DataForMainPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.List;
-
 @Controller
 public class RootController {
 
     @Autowired
-    private TrainingService trainingService;
+    private DataForMainPageService dataForMainPageService;
 
     @RequestMapping("/")
     public String index(Model model) {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
-        Calendar calendar = Calendar.getInstance();
-        model.addAttribute("today", dateFormat.format(calendar.getTime()));
-
-        List<TrainingSession> listOfTrainingSessions = trainingService.findAllTrainingSessions();
-        model.addAttribute("listOfTrainingSessions", listOfTrainingSessions);
-
-        List<TrainingSession> lastThreeTrainingSessions = TrainingSessionListManupulation.listOutLastThreeTrainingsSessins(listOfTrainingSessions);
-        model.addAttribute("lastThreeTrainingSessions",lastThreeTrainingSessions);
+        model.addAllAttributes(dataForMainPageService.getTrainingSessionData());
+        model.addAllAttributes(dataForMainPageService.getTodayDate());
 
         return "index";
     }
